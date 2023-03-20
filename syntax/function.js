ae58d3803ab28c937c5fb62e6e2905046530f902
Clemens <kris0410@kakao.com>
2023-03-20 20:04:28 +0900

    study.. I'm sleepy

--- syntax/function.js
+++ syntax/function.js
@@ -2,8 +2,10 @@ console.log(Math.round(1.6));
 console.log(Math.round(1.4));
 
 function sum(first, second){ // parameter
-
+    return first + second;
 }
+
+var a = sum(1., 2);
 
 this.sum(1, 1); // argument
 
