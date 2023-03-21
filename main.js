f0eab8652adb2a26672345537dd7936c55596efe
Clemens <kris0410@kakao.com>
2023-03-21 19:25:07 +0900

    study

--- main.js
+++ main.js
@@ -7,37 +7,42 @@ var app = http.createServer(function (request, response) {
   var queryData = url.parse(_url, true).query;
   var pathName = url.parse(_url, true).pathname;
 
-  // console.log(pathName);
+  function templateHTML(title, list, body){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+    </html>
+    `
+  }
+  function templateList(filelist) {
+    var list = `<ul>`;
+    var i = 0;
+    while (i < filelist.length) {
+      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+      i += 1;
+    }
+    list += `</ul>`;
+
+    return list;
+  }
+
   if (pathName === '/') {
     if (queryData.id == undefined) {
       fs.readdir('./data', function (error, filelist) {
-        console.log(filelist);
+        // console.log(filelist);
         var title = 'Welcome';
         var description = 'Hello, Node.js';
-        var list = `<ul>`;
-        var i = 0;
-        while (i < filelist.length) {
-          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i += 1;
-        }
-        list += `</ul>`;
-        var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>
-            ${description}
-            </p>
-          </body>
-          </html>
-          `
+        var list = templateList(filelist)
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
         // response.end(fs.readFileSync(__dirname + _url));
         // response.end(queryData.id);
         response.writeHead(200); // 200 파일 전송 성공
@@ -45,36 +50,11 @@ var app = http.createServer(function (request, response) {
       })
     } else {
       fs.readdir('./data', function (error, filelist) {
-        console.log(filelist);
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = `<ul>`;
-        var i = 0;
-        while (i < filelist.length) {
-          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i += 1;
-        }
-        list += `</ul>`;
-
+        // console.log(filelist);
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>
-          ${description}
-          </p>
-        </body>
-        </html>
-        `
+          var list = templateList(filelist)
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           // response.end(fs.readFileSync(__dirname + _url));
           // response.end(queryData.id);
           response.writeHead(200); // 200 파일 전송 성공
